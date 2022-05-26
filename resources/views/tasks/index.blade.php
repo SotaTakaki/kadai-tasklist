702840fd0913bb7b6db8aaf76736e3cad4f8990a
Sota Takaki <sota-takaki@dac.co.jp>
2022-05-26 00:39:57 +0000

    finish tasklist

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,32 @@
+@extends("layouts.App")
+
+@section("content")
+
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route("tasks.show", $task->id, ["task" => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {!! link_to_route("tasks.create", "新規タスクの投稿", [], ["class" => "btn btn-primary"]) !!}
+    
+    {!! Form::model("$task", ["route" => ["tasks.destroy", $task->id], "method" => "delete"]) !!}
+        {!! Form::submit("削除", ["class" => "btn btn-danger"]) !!}
+    {!! Form::close() !!}
+    
+@endsection
