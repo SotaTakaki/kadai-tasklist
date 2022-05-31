0b1348d89c22561fedd6ee0ade06dd6365e875ba
Sota Takaki <sota-takaki@dac.co.jp>
2022-05-31 06:50:37 +0000

    user registration

--- resources/views/Common/header.blade.php
+++ resources/views/Common/header.blade.php
@@ -6,7 +6,14 @@
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+        
+        <ul class="navbar-nav">
+            {{-- ユーザ登録ページへのリンク --}}
+            <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+            {{-- ログインページへのリンク --}}
+            <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        </ul>
+                        
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
